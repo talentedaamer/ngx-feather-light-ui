cb67b152075c3244adc8cb7cc0487ed2e7173e09
Aamer Shahzad <a.shahzad@id-ware.com>
2022-08-30 14:14:09 +0500

    card directive test error fix

--- projects/ngx-feather-light-ui/src/lib/flui-directives/flui-card.directive.spec.ts
+++ projects/ngx-feather-light-ui/src/lib/flui-directives/flui-card.directive.spec.ts
@@ -1,8 +1,13 @@
 import { FluiCardDirective } from './flui-card.directive';
+import {ElementRef} from "@angular/core";
 
 describe('FluiCardDirective', () => {
+  const elementRefMock = {
+    nativeElement: document.createElement('div')
+  }
+
   it('should create an instance', () => {
-    const directive = new FluiCardDirective();
+    const directive = new FluiCardDirective(elementRefMock);
     expect(directive).toBeTruthy();
   });
 });
